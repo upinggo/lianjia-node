4a51ff7a21646547429c92f3e126d45957d66880
Neil.Shen <373162182@qq.com>
2020-08-06 02:10:17 +0800

    add

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path')
 const webpack = require('webpack');
 module.exports = {
-    output:__dirname+'/public',
-    plugins: [
-        new CopyWebpackPlugin([
-            {
-                from: 'public/js/*.js',
-                to: path.resolve(__dirname, 'dist', 'txt'),
-            },
-            //还可以继续配置其它要拷贝的文件
-        ])
-    ]
+    output:{
+        path: path.resolve(__dirname, "dist"), 
+    }
+    // plugins: [
+        // new CopyWebpackPlugin([
+        //     {
+        //         from: 'public/js/*.js',
+        //         to: path.resolve(__dirname, 'dist', 'txt'),
+        //     },
+        //     //还可以继续配置其它要拷贝的文件
+        // ])
+    // ]
 }
